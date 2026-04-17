74b4fed61bba99b526e213a35898a9266ad80ca5
pgms1103-afk <pgms1103@gmail.com>
2026-04-16 21:49:13 -0500

    Creation
    I create the proyect

---
+++ detectia/detectia/src/main/java/co/edu/unbosque/detectia/DetectiaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.detectia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectiaApplication.class, args);
+	}
+
+}
