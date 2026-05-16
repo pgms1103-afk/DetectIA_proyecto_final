2171f74082dcdd7292afd2206e48bc6c2c6aa231
pgms1103-afk <pgms1103@gmail.com>
2026-05-16 17:16:04 -0500

    Nuevas APIs
    Se añadio una nueva IA de texto e imagenes, la diferencia es que en imagenes, no se sube localmente, si no por medio de una URL, todo funcional.

--- detectia/detectia/src/main/java/co/edu/unbosque/detectia/service/ArchivoService.java
+++ detectia/detectia/src/main/java/co/edu/unbosque/detectia/service/ArchivoService.java
@@ -1,14 +1,12 @@
 package co.edu.unbosque.detectia.service;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import co.edu.unbosque.detectia.dto.ArchivoDTO;
 import co.edu.unbosque.detectia.entity.Archivo;
 import co.edu.unbosque.detectia.entity.ResultadoIA;
@@ -25,31 +23,38 @@ public class ArchivoService implements CRUDoperation<ArchivoDTO>{
 	
 	@Autowired
 	private UsuarioRepository usuarioRepo;
+
 	
 	@Autowired
 	private ModelMapper mapper;
 	
-	@Autowired
-	private ResultadoIARepository resultadoIARepo;
-	
-	public ArchivoService() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 	@Override
 	public int create(ArchivoDTO data) {
-		Archivo entity = mapper.map(data, Archivo.class);
-		entity.setId(null);
-		entity.setFechaSubida(LocalDateTime.now());
-		archivoRepo.save(entity);
-		return 0;
+		 Optional<Usuario> encontrado = usuarioRepo.findById(data.getUsuarioId());
+	        if (encontrado.isEmpty()) {
+	            return 1;
+	        } else {
+	        	Archivo entity = mapper.map(data, Archivo.class);
+	    		archivoRepo.save(entity);
+	    		return 0;
+	        }
+		
 	}
 	
 	public Archivo createAndReturn(ArchivoDTO data) {
-		Archivo entity = mapper.map(data, Archivo.class);
-		entity.setId(null);
-		entity.setFechaSubida(LocalDateTime.now());
-		return archivoRepo.save(entity);
+	    Optional<Usuario> encontrado = usuarioRepo.findById(data.getUsuarioId());
+	    if (encontrado.isEmpty()) {
+	        return null;
+	    } else {
+	        Archivo entity = new Archivo();
+	        entity.setNombre(data.getNombre());
+	        entity.setRutaAlmacenamiento(data.getRutaAlmacenamiento());
+	        entity.setFechaSubida(data.getFechaSubida());
+	        entity.setUsuario(encontrado.get()); // ← relación correctamente seteada
+	        return archivoRepo.save(entity);
+	    }
 	}
 
 	@Override
@@ -57,50 +62,45 @@ public class ArchivoService implements CRUDoperation<ArchivoDTO>{
 		List<Archivo> entityList = (List<Archivo>) archivoRepo.findAll();
 		List<ArchivoDTO> dtoList = new ArrayList<>();
 		entityList.forEach((entity) -> {
-			ArchivoDTO dto = mapper.map(entity, ArchivoDTO.class);
+			ArchivoDTO dto = new ArchivoDTO();
+			dto.setId(entity.getId());
+			dto.setNombre(entity.getNombre());
+			dto.setRutaAlmacenamiento(entity.getRutaAlmacenamiento());
+			dto.setFechaSubida(entity.getFechaSubida());
+			dto.setUsername(entity.getUsuario() != null ? entity.getUsuario().getNombreUsuario(): null);
+			dto.setUsuarioId(entity.getUsuario() != null ? entity.getUsuario().getId(): null);
 			dtoList.add(dto);
 		});
-		
 		return dtoList;
-		
 	}
 
 	@Override
 	public int delateById(Long id) {
 		Optional<Archivo> encontrado = archivoRepo.findById(id);
 		if(encontrado.isPresent()) {
-			
-			List<ResultadoIA> resultados = resultadoIARepo.findByArchivo(encontrado.get());
-			ArchivoDTO dto =mapper.map(encontrado.get(), ArchivoDTO.class);
-			Archivo entity = mapper.map(dto, Archivo.class);
-			resultadoIARepo.deleteAll(resultados);
-			archivoRepo.delete(entity);
+			archivoRepo.delete(encontrado.get());
 			return 0;
+		}else {
+			return 1;
 		}
-		
-		return 1;
 	}
 
 	@Override
 	public int updateById(Long id, ArchivoDTO data) {
 		Optional<Archivo> encontrado = (Optional<Archivo>) archivoRepo.findById(id);
-		
 		if(encontrado.isPresent()) {
 			Archivo temp = encontrado.get();
 			temp.setNombre(data.getNombre());
-			temp.setTipo(data.getTipo());
 			temp.setFechaSubida(data.getFechaSubida());
 			temp.setRutaAlmacenamiento(data.getRutaAlmacenamiento());
 			archivoRepo.save(temp);
 			return 0;
-		}
-		
+		}	
 		return 1;
-		
 	}
 	
-	public List<ArchivoDTO> getArchivosByCorreo(String correo){
-		Optional<Usuario> usuarioEncontrado = usuarioRepo.findByCorreo(correo);
+	public List<ArchivoDTO> getArchivosByuser(String username){
+		Optional<Usuario> usuarioEncontrado = usuarioRepo.findByNombreUsuario(username);
 		if(usuarioEncontrado.isEmpty()) {
 			return new ArrayList<>();
 		}
@@ -108,7 +108,13 @@ public class ArchivoService implements CRUDoperation<ArchivoDTO>{
 		List<Archivo> entityList = archivoRepo.findByUsuario(usuarioEncontrado.get());
 		List<ArchivoDTO> dtoList = new ArrayList<>();
 		entityList.forEach((entity) -> {
-			ArchivoDTO dto = mapper.map(entity, ArchivoDTO.class);
+			ArchivoDTO dto = new ArchivoDTO();
+			dto.setId(entity.getId());
+			dto.setNombre(entity.getNombre());
+			dto.setRutaAlmacenamiento(entity.getRutaAlmacenamiento());
+			dto.setFechaSubida(entity.getFechaSubida());
+			dto.setUsername(entity.getUsuario() != null ? entity.getUsuario().getNombreUsuario(): null);
+			dto.setUsuarioId(entity.getUsuario() != null ? entity.getUsuario().getId(): null);
 			dtoList.add(dto);
 		});
 		
@@ -124,5 +130,8 @@ public class ArchivoService implements CRUDoperation<ArchivoDTO>{
 	}
 	
 	
+	}
 	
-}
+	
+	
+
--- detectia/detectia/src/main/java/co/edu/unbosque/detectia/service/EleccionService.java
+++ detectia/detectia/src/main/java/co/edu/unbosque/detectia/service/EleccionService.java
@@ -1,5 +1,6 @@
 package co.edu.unbosque.detectia.service;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,11 +33,11 @@ public class EleccionService {
 
 	@Autowired
 	private HuggingFaceService huggingFace;
-	
+
 	@Autowired
 	private MistralService mistral;
-	
-	@Autowired 
+
+	@Autowired
 	private SightengineService sightengine;
 
 	@Autowired
@@ -45,18 +46,34 @@ public class EleccionService {
 	@Autowired
 	private HiveVideoService hiveVideo;
 
+	@Autowired
+	private WinstonService winston;
+
 	public Map<String, Double> analizar(MultipartFile archivo) throws Exception {
-	    String tipo = extractor.detectarTipo(archivo);
+		String tipo = extractor.detectarTipo(archivo);
+
+		if (tipo.contains("pdf")) {
+			return analizarPDF(archivo);
+		} else if (esDocumento(tipo)) {
+			return analizarTexto(archivo);
+		} else if (tipo.startsWith("image")) {
+			return analizarImagen(archivo);
+		} else if (tipo.startsWith("video")) {
+			return analizarVideo(archivo);
+		}
+		return new HashMap<>();
+	}
+	
+	public Map<String, Double> analizar(String url) throws Exception {
+	    String urlMinuscula = url.toLowerCase();
 	    
-	    if (tipo.contains("pdf")) {
-	        return analizarPDF(archivo);
-	    } else if (esDocumento(tipo)) {
-	        return analizarTexto(archivo);
-	    }else if (tipo.startsWith("image")){
-	    	return analizarImagen(archivo);
-	    }else if (tipo.startsWith("video")) { 
-	        return analizarVideo(archivo);
+	    if (urlMinuscula.endsWith(".jpg") || urlMinuscula.endsWith(".jpeg") || 
+	        urlMinuscula.endsWith(".png") || urlMinuscula.endsWith(".webp")) {
+	        
+	        return analizarImagenURL(url);
 	    }
+	    
+	    // Si en el futuro manejas URLs de videos o páginas web, pondrías los otros else if aquí
 	    return new HashMap<>();
 	}
 
@@ -65,68 +82,69 @@ public class EleccionService {
 
 		System.out.println("TEXTO EXTRAIDO: [" + texto + "]");
 		System.out.println("LONGITUD: " + texto.length());
-		
 
 		Map<String, Double> resultadosIA = new HashMap<>();
-			
-			// Invocación a las APIs de texto
-			//resultadosIA.put("ZeroGPT", zeroGPT.detectarIA(texto).getData().getIs_gpt_generated());
-			resultadosIA.put("Grok", grok.detectarIA(texto).getPorcentajeIA());
-			resultadosIA.put("Gemini", gemini.detectarIA(archivo.getBytes(), archivo.getContentType()).getPorcentajeIA());
-			resultadosIA.put("HuggingFace", huggingFace.detectarIA(texto).getPorcentajeIA());
-			resultadosIA.put("Mistral", mistral.detectarIA(texto, archivo.getBytes(), archivo.getContentType()).getFakePercentage());
 
-			return resultadosIA;
+		// Invocación a las APIs de texto
+		//resultadosIA.put("ZeroGPT", zeroGPT.detectarIA(texto).getData().getIs_gpt_generated());
+		/*SIRVE*/resultadosIA.put("Grok", grok.detectarIA(texto).getPorcentajeIA());
+		/*SIRVE*/resultadosIA.put("Gemini", gemini.detectarIA(archivo.getBytes(), archivo.getContentType()).getPorcentajeIA());
+		/*pienso en quitarla, muy malos resultados*/resultadosIA.put("HuggingFace", huggingFace.detectarIA(texto).getPorcentajeIA());
+		/*SIRVE*/resultadosIA.put("Mistral",mistral.detectarIA(texto, archivo.getBytes(), archivo.getContentType()).getFakePercentage());
+		/*SIRVE*/resultadosIA.put("Winstral", winston.detectarIA(texto).getScore());
+		return resultadosIA;
+	}
+
+	private Map<String, Double> analizarPDF(MultipartFile archivo) throws Exception {
+		String texto = extractor.extraerTexto(archivo);
+		byte[] bytes = archivo.getBytes();
+		String mimeType = archivo.getContentType();
+
+		Map<String, Double> resultadosIA = new HashMap<>();
+
+		// Gemini y Mistral reciben los bytes completos del PDF (analizan texto +
+		// imágenes)
+		resultadosIA.put("Gemini", gemini.detectarIA(bytes, mimeType).getPorcentajeIA());
+
+		// Groq y HuggingFace solo pueden analizar el texto extraído
+		if (texto != null && !texto.isBlank()) {
+			/*SIRVE*/resultadosIA.put("Grok", grok.detectarIA(texto).getPorcentajeIA());
+			/*pienso en quitarla, muy malos resultados*/resultadosIA.put("HuggingFace", huggingFace.detectarIA(texto).getPorcentajeIA());
+			/*SIRVE*/resultadosIA.put("Mistral", mistral.detectarIA(texto, bytes, mimeType).getFakePercentage());
+			/*SIRVE*/resultadosIA.put("Winstral", winston.detectarIA(texto).getScore());
 		}
-		
-		private Map<String, Double> analizarPDF(MultipartFile archivo) throws Exception {
-		    String texto = extractor.extraerTexto(archivo);
-		    byte[] bytes = archivo.getBytes();
-		    String mimeType = archivo.getContentType();
-		    
-		    Map<String, Double> resultadosIA = new HashMap<>();
-
-		    // Gemini y Mistral reciben los bytes completos del PDF (analizan texto + imágenes)
-		    resultadosIA.put("Gemini", gemini.detectarIA(bytes, mimeType).getPorcentajeIA());
-
-
-		    // Groq y HuggingFace solo pueden analizar el texto extraído
-		    if (texto != null && !texto.isBlank()) {
-		        resultadosIA.put("Grok", grok.detectarIA(texto).getPorcentajeIA());
-		        resultadosIA.put("HuggingFace", huggingFace.detectarIA(texto).getPorcentajeIA());
-			    resultadosIA.put("Mistral", mistral.detectarIA(texto, bytes, mimeType).getFakePercentage());
-		    }
-
-		    return resultadosIA;
-		}
-
-
-		// 🖼 IMAGEN (Ajustado para retornar Map)
-		// ===============================
-
-		private Map<String, Double> analizarImagen(MultipartFile archivo) throws Exception {
-			Map<String, Double> resultadosIA = new HashMap<>();
-			byte[] bytes = archivo.getBytes();
-			String mimeType = archivo.getContentType();
-			resultadosIA.put("Sightengine", sightengine.detectarIA(bytes, mimeType ).getAi_generated());
-			resultadosIA.put("Gemini", gemini.detectarIA(bytes, mimeType).getPorcentajeIA());
-
 
 		return resultadosIA;
 	}
-	
+
+	// 🖼 IMAGEN (Ajustado para retornar Map)
+	// ===============================
+
+	private Map<String, Double> analizarImagen(MultipartFile archivo) throws Exception {
+		Map<String, Double> resultadosIA = new HashMap<>();
+		byte[] bytes = archivo.getBytes();
+		String mimeType = archivo.getContentType();
+		/*SIRVE*/resultadosIA.put("Sightengine", sightengine.detectarIA(bytes, mimeType).getAi_generated());
+		/*SIRVE*/resultadosIA.put("Gemini", gemini.detectarIA(bytes, mimeType).getPorcentajeIA());
+
+		return resultadosIA;
+	}
+
+	private Map<String, Double> analizarImagenURL(String url) throws IOException, InterruptedException {
+		Map<String, Double> resultadosIA = new HashMap<>();
+		/*SIRVE*/resultadosIA.put("Winstral", winston.detectarIAImagen(url).getScore());
+		
+		return resultadosIA;
+	}
+
 	private Map<String, Double> analizarVideo(MultipartFile archivo) throws Exception {
 		Map<String, Double> resultadosIA = new HashMap<>();
 
-		TwelveLabsDTO tl = twelveLabs.detectarIA(
-				archivo.getBytes(),
-				archivo.getOriginalFilename(),
+		TwelveLabsDTO tl = twelveLabs.detectarIA(archivo.getBytes(), archivo.getOriginalFilename(),
 				archivo.getContentType());
 		resultadosIA.put("TwelveLabs", tl.getPorcentajeIA());
 
-		HiveVideoDTO hv = hiveVideo.detectarIA(
-				archivo.getBytes(),
-				archivo.getOriginalFilename(),
+		HiveVideoDTO hv = hiveVideo.detectarIA(archivo.getBytes(), archivo.getOriginalFilename(),
 				archivo.getContentType());
 		resultadosIA.put("HiveVideo", hv.getPorcentajeIA());
 
@@ -136,8 +154,8 @@ public class EleccionService {
 	// ===============================
 	// 🔍 HELPERS
 	// ===============================
-		private boolean esDocumento(String tipo) {
-		    return tipo.startsWith("text") || tipo.contains("word");
-		}
+	private boolean esDocumento(String tipo) {
+		return tipo.startsWith("text") || tipo.contains("word");
+	}
 
 }
