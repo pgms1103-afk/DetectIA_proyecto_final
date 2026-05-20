c44d0cdd3add5815c9c349f4e35ba417b16b3934
pgms1103-afk <pgms1103@gmail.com>
2026-05-20 10:45:45 -0500

    Avances
    Se conecto y quedo funcional la lógica de subir archivos y de ver sus resultaados por IA y analisis total.
    Tambien se muestran los usuarios en el panel de admin.

--- frontDetectIA/src/app/services/usuario.service.ts
+++ frontDetectIA/src/app/services/usuario.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
-import { Subject } from 'rxjs'
+import { Observable, Subject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
@@ -11,4 +11,32 @@ export class UsuarioService {
 
   private cliente: HttpClient = inject(HttpClient);
   private readonly urlbase: String = 'http://localhost:8080/admin'
+
+  private refrescarTabla = new Subject<void>();
+
+  public listaUsuarios = new Subject<UsuarioModel[]>();
+  listausuarios$ = this.listaUsuarios.asObservable();
+
+  private tipoFiltro = new Subject<string>();
+  tipoFiltro$ = this.tipoFiltro.asObservable();
+
+  postCrearUsuario(usuario: UsuarioModel):Observable<string>{
+    return this.cliente.post(this.urlbase + '/crearUsuario', usuario, {responseType: 'text'});
+  }
+
+  getMostrarUsuarios(): Observable<UsuarioModel[]>{
+    return this.cliente.get<UsuarioModel[]>(this.urlbase + '/mostrarusuarios',)
+  }
+
+  putActualizarUsuario(id:number, usuario:UsuarioModel): Observable<string>{
+    return this.cliente.put(this.urlbase+'/actualizarusuarios', usuario, {responseType: 'text'});
+  }
+
+  deleteUsuarios(id:number): Observable<string>{
+    const parametros = new HttpParams().set('id', id.toString());
+    return this.cliente.delete(`${this.urlbase}/eliminarusuarios`, {
+      params: parametros,
+      responseType: 'text'
+    });
+  }
 }
--- frontDetectIA/src/app/vista-admin/gestion-usuarios/gestion-usuarios.ts
+++ frontDetectIA/src/app/vista-admin/gestion-usuarios/gestion-usuarios.ts
@@ -1,20 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsuarioModel } from '../../models/usuario.model';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-gestion-usuarios',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './gestion-usuarios.html',
-  styleUrl: './gestion-usuarios.css'
+  styleUrl: './gestion-usuarios.css',
 })
-export class GestionUsuarios {
+export class GestionUsuarios implements OnInit {
+
+  usuarios: UsuarioModel[] = [];
+  usuarioService: UsuarioService = inject(UsuarioService);
+
+  ngOnInit(): void {
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios() {
+    this.usuarioService.getMostrarUsuarios().subscribe({
+      next: (datos) =>{
+        this.usuarios = datos;
+        console.log(this.usuarios);
+      },
+      error: (e) => {
+        console.error("Algo fallo");
+      }
+    })
+  }
 
   listaUsuarios = [
-    { id: 1, nombre: 'Jose Manuel Toro', correo: 'jose.toro@unbosque.edu.co', rol: 'Admin', activo: true, fecha: '12 May 2026' },
-    { id: 2, nombre: 'Nelson Cipagauta', correo: 'nelson.c@email.com', rol: 'Usuario', activo: true, fecha: '10 May 2026' },
-    { id: 3, nombre: 'Gloria Toro', correo: 'gloria.t@email.com', rol: 'Usuario', activo: false, fecha: '01 May 2026' },
-    { id: 4, nombre: 'Yireth Fonseca', correo: 'yireth.f@email.com', rol: 'Usuario', activo: true, fecha: '28 Abr 2026' },
+    {
+      id: 1,
+      nombre: 'Jose Manuel Toro',
+      correo: 'jose.toro@unbosque.edu.co',
+      rol: 'Admin',
+      activo: true,
+      fecha: '12 May 2026',
+    },
+    {
+      id: 2,
+      nombre: 'Nelson Cipagauta',
+      correo: 'nelson.c@email.com',
+      rol: 'Usuario',
+      activo: true,
+      fecha: '10 May 2026',
+    },
+    {
+      id: 3,
+      nombre: 'Gloria Toro',
+      correo: 'gloria.t@email.com',
+      rol: 'Usuario',
+      activo: false,
+      fecha: '01 May 2026',
+    },
+    {
+      id: 4,
+      nombre: 'Yireth Fonseca',
+      correo: 'yireth.f@email.com',
+      rol: 'Usuario',
+      activo: true,
+      fecha: '28 Abr 2026',
+    },
   ];
 
   mostrarModal = false;
@@ -42,4 +91,6 @@ export class GestionUsuarios {
   simularEliminar() {
     console.log('Aquí se enviará la petición DELETE a Spring Boot');
   }
+
+
 }
