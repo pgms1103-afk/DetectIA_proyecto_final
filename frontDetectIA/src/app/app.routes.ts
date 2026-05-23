d54f7be37ff592adb031626b543eb767270b5a33
JMCip <josemanuelcipa2006@gmail.com>
2026-05-23 02:33:56 -0500

    pestañas sin links martin gay hijueputa

--- frontDetectIA/src/app/app.routes.ts
+++ frontDetectIA/src/app/app.routes.ts
@@ -1,11 +1,32 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
-import { VistaUsuario } from './vista-usuario/vista-usuario';
 import { VistaAdmin } from './vista-admin/vista-admin';
+import { VistaUsuario } from './vista-usuario/vista-usuario';
+import { adminGuard, usuarioGuard } from './guards/role.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: Login },
-  { path: 'usuario', component: VistaUsuario },
-  { path: 'admin', component: VistaAdmin },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'admin',
+    component: VistaAdmin,
+    canActivate: [adminGuard]
+  },
+  {
+    path: 'usuario',
+    component: VistaUsuario,
+    canActivate: [usuarioGuard]
+  },
+  {
+
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
