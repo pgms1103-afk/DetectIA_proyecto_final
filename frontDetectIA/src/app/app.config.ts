ab6c5607c7ec8f9a6b30df043d2023c4c5da2e2e
JMCip <josemanuelcipa2006@gmail.com>
2026-05-23 01:59:53 -0500

    Pantalla de subida de archivos
    martin malparido

--- frontDetectIA/src/app/app.config.ts
+++ frontDetectIA/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { ToastrModule} from 'ngx-toastr';
 import { provideAnimations} from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
@@ -12,12 +11,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
-    importProvidersFrom(
-      ToastrModule.forRoot({
-        timeOut: 5000,
-        positionClass: 'toast-top-right',
-        preventDuplicates: true,
-      })
-    ),
   ],
 };
